b115bfd1d21886124b60493009bb8a1e8da62413
Faeiz Mahrus <167124477+faeizmahrus@users.noreply.github.com>
2024-12-25 23:07:40 +0600

    feat: Refactor to use scripts (#88)
    * Refactor to allow multiple image builds
    
    * Don't build fedora-example
    
    * Refactor to use scripts and packagelists
    
    ---------
    
    Co-authored-by: Jorge O. Castro <jorge.castro@gmail.com>

---
+++ scripts/boxkit.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+
+# Symlink distrobox shims
+./distrobox-shims.sh
+
+# Update the container and install packages
+apk update && apk upgrade
+grep -v '^#' ./boxkit.packages | xargs apk add
---
+++ scripts/fedora-example.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+
+# Symlink distrobox shims
+./distrobox-shims.sh
+
+# Update the container and install packages
+dnf update -y
+grep -v '^#' ./fedora-example.packages | xargs dnf install -y 
