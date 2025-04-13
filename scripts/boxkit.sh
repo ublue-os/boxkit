6b556bf0e387e6c881b353a15950b625731c8394
Ben Robertson <benrobertson150@hotmail.co.uk>
2025-04-13 02:48:30 +0100

    Enable testing repository behind @testing tag. Install bitwarden-cli through npm.

--- scripts/boxkit.sh
+++ scripts/boxkit.sh
@@ -3,7 +3,11 @@
 # Symlink distrobox shims
 ./distrobox-shims.sh
 
+#Add testing repository
+echo "@testing http://dl-cdn.alpinelinux.org/alpine/edge/testing" >> /etc/apk/repositories
+
 # Update the container and install packages
 apk update && apk upgrade
 grep -v '^#' ./boxkit.packages | xargs apk add
+#Install bitwarden-cli
 npm install -g @bitwarden/cli
